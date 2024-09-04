bdd92a439fd1bf026bf1b4b38eecfa6b186b4ce0
Rs0309 <156988245+Rs0309@users.noreply.github.com>
2024-09-05 01:42:50 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+document.getElementById('studentBtn').addEventListener('click', function() {
+     window.location.href = 'student/st_login.php';
+});
+
+document.getElementById('adminBtn').addEventListener('click', function() {
+    window.location.href = 'admin/ad_login.php';
+});
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Welcome</h1>
+        <div class="button-container">
+            <button id="studentBtn">Log in as Student</button>
+            
+            <button id="adminBtn">Log in as Administrator</button>
+        </div>
+    </div>
+    <script src="login.js"></script>
+</body>
+</html>
